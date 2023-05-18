afa53c0fcb6b0f90ae254c5812160b76bc432d93
Chris Fretz <cfretz@icloud.com>
2023-05-18 13:30:19 -0400

    removed unnecessary stuff and switched to bit_cast

--- varvec.cpp
+++ varvec.cpp
@@ -1,3 +1,4 @@
+#include <bit>
 #include <iostream>
 #include <variant>
 #include <string>
@@ -41,54 +42,6 @@ template <meta::trivially_swappable, size_t, size_t>
 class variant_array;
 
 namespace detail {
-
-  // Base case, unimplemented.
-  template <class>
-  struct variant_unpacker;
-
-  // Partial specialization to be able to extract the variant's types
-  template <template <class...> class Variant, class... Types>
-  struct variant_unpacker<Variant<Types...>> {
-    using variant_type = Variant<Types...>;
-
-    // Try each type, one after another, peeling one off on each recursion.
-    template <class T, class... Ts, size_t Idx, size_t... Idxs>
-    static constexpr variant_type unpack_impl(uint8_t type_idx,
-        uint8_t const* repr, std::index_sequence<Idx, Idxs...>) {
-      constexpr size_t curr_idx = Idx;
-      using current_type = std::variant_alternative_t<curr_idx, variant_type>;
-
-      // Is the current index equal to our recorded type index?
-      if (type_idx == curr_idx) {
-        // Create temporary aligned storage on the stack and return that
-        // This allows us to keep the data structure size minimal, without accidentally
-        // trying to load misaligned floats, for the low cost of one copy.
-        std::aligned_storage_t<sizeof(std::max_align_t), alignof(std::max_align_t)> tmp;
-        
-        // New is a no-op, as this is a trivial type, but I think it technically makes the
-        // std::launder below well defined behavior.
-        new(&tmp) current_type();
-        memcpy(&tmp, repr, sizeof(current_type));
-
-        // Launder the pointer to get a live object to return.
-        return *std::launder(reinterpret_cast<current_type*>(&tmp));
-      }
-
-      // If there are types left to try, recurse, otherwise this is unreachable.
-      // Clearly tail recursion, the entire call stack should get inlined and be a series
-      // of conditional integer comparisons
-      if constexpr (sizeof...(Ts)) {
-        return unpack_impl<Ts...>(type_idx, repr, std::index_sequence<Idxs...> {});
-      } else {
-        __builtin_unreachable();
-      }
-    }
-
-    // Landing function used to start the recursive implementation
-    static constexpr variant_type unpack(uint8_t const* data) {
-      return unpack_impl<Types...>(*data, data + 1, std::index_sequence_for<Types...> {});
-    }
-  };
 
   template <class Container>
   class vararray_iterator {
@@ -223,9 +176,9 @@ namespace detail {
     noexcept(noexcept(std::forward<Func>(callback)(std::declval<T&>())))
   {
     return with_aligned_stack_storage<T>([&] (auto& storage) {
-      T* tmp = new(&storage) T();
-      memcpy(tmp, data, sizeof(T));
-      return std::forward<Func>(callback)(*tmp);
+      // This is now well defined behavior in C++20
+      memcpy(&storage, data, sizeof(T));
+      return std::forward<Func>(callback)(*std::launder(reinterpret_cast<T*>(&storage)));
     });
   }
 
@@ -235,7 +188,7 @@ namespace detail {
     noexcept((noexcept(std::forward<Func>(callback)(std::declval<Ts&>())) && ...))
   {
     if (idx == type_index) {
-      if (reinterpret_cast<std::uintptr_t>(data) & (alignof(T) - 1)) {
+      if (std::bit_cast<std::uintptr_t>(data) & (alignof(T) - 1)) {
         // Pointer is misaligned, fix it up
         return unpack_misaligned_type<T>(data, std::forward<Func>(callback));
       } else {
@@ -353,6 +306,11 @@ namespace detail {
     storage_type data {0};
   };
 
+  template <class T>
+  constexpr bool aligned_for_type(void const* ptr) {
+    return !(std::bit_cast<std::uintptr_t>(ptr) & (alignof(T) - 1));
+  }
+
   template <class Variant, size_t MaxItems, size_t StorageSize>
   struct autotrivial_variant_base : std::conditional_t<
     std::is_trivially_destructible_v<Variant>,
@@ -397,10 +355,10 @@ class variant_array : private detail::autotrivial_variant_base<Variant, MaxItems
 
         // Copy
         auto* const data_ptr = this->get_data() + offset;
-        if (reinterpret_cast<std::uintptr_t>(data_ptr) & (alignof(stored_type) - 1)) {
-          memcpy(data_ptr, &arg, sizeof(stored_type));
-        } else {
+        if (detail::aligned_for_type<stored_type>(data_ptr)) {
           new(data_ptr) stored_type(std::forward<T>(arg));
+        } else {
+          memcpy(data_ptr, &arg, sizeof(stored_type));
         }
         this->incr_offset(sizeof(stored_type));
       }, val);
